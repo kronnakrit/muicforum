cf32bbee44f49cdad9809c48350201bddf6a1f3b
Ronnakrit Kunaviriyasiri <r.kunaviriyasiri@gmail.com>
2014-02-14 16:44:41 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MuicForum::Application.config.secret_key_base = '88bdc413f8815bc61f48dd3528ef576a8523c01c32b95e2ef1545e82bdaa4f7d4ff1dc3188198939cf35c0cc3fb210d039a0253630db5199a6c27d53a32b7e5b'
