bf6fd800582229b9ac0e56ffaaab9d26665edde7
Ronnakrit Kunaviriyasiri <r.kunaviriyasiri@gmail.com>
2014-03-05 14:10:32 +0700

    finish first css

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
-  default_scope -> { order('created_at DESC') }
+  default_scope -> { order('created_at ASC') }
   validates :user_id, presence: true
 end
