bf1ab14d5974ac1f0a039be2b48a93baf9ba4fe6
Ronnakrit Kunaviriyasiri <r.kunaviriyasiri@gmail.com>
2014-03-30 00:17:48 +0700

    remove simple capcha

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-SimpleCaptcha.store = 'redis'
 
 module MuicForum
   class Application < Rails::Application
